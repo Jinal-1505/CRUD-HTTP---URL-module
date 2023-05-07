a56c9e0aac06875a9f2592534a7fa1344931a2b8
Jinal Mistry <jinalmistry1505@gmail.com>
2023-05-07 19:19:27 +0530

    ini

---
+++ index.js
@@ -0,0 +1,109 @@
+const http = require("http");
+const url = require('url');
+const queryString = require('querystring');
+const host = "localhost";
+const port = "3000";
+const users = [
+    { email: "abc@gmail.com", name: "abc", age: 20 },
+    { email: "jinny@gmail.com", name: "Jinny", age: 22 }
+]
+const server = http.createServer((req, res) => {
+    // const method = req.method;
+    const { pathname, query } = url.parse(req.url);
+    const { email } = queryString.parse(query);
+
+    // Handle GET requests
+    switch (req.method) {
+        case 'GET':
+            if (pathname === '/') {
+                res.setHeader('Content-Type', 'application/json');
+                res.writeHead(200);
+                res.end(JSON.stringify(users));
+            }
+            else if (pathname === '/user' && email) {
+                const user = users.find(u => u.email);
+                if (user) {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.writeHead(200);
+                    res.end(JSON.stringify(user));
+                }
+                else {
+                    return handleError(res, 404);
+                }
+            }
+            else {
+                return handleError(res, 404);
+            }
+            break;
+        case 'POST':
+            if (pathname === '/') {
+                let b = '';
+                req.on('data', chunks => {
+                    b += chunks.toString();
+                });
+                req.on('end', () => {
+                    const user = JSON.parse(b);
+                    if (users.find(u => u.email === user.email)) {
+                        res.writeHead(409, { 'Content-Type': 'application/json' });
+                        res.end();
+                    }
+                    else {
+                        users.push(user);
+                        res.writeHead(200, { 'Content-Type': 'application/json' });
+                        res.end();
+                    }
+                });
+            } else {
+                return handleError(res, 404);
+            }
+            break;
+        case 'PUT':
+            if (pathname === '/user' && email) {
+                let b = '';
+                req.on('data', chunks => {
+                    b = b + chunks.toString();
+                });
+                req.on('end', () => {
+                    const user = JSON.parse(b);
+                    const index = users.findIndex(u => u.email === email);
+                    if (index >= 0) {
+                        users[index] = { ...user, email }
+                        res.writeHead(200, { 'Content-Type': 'application/json' });
+                        res.end();
+                    } else {
+                        return handleError(res, 404);
+                    }
+
+                });
+            }
+            else {
+                return handleError(res, 404);
+            }
+            break;
+        case 'DELETE':
+            if (pathname === '/user' && email) {
+                const index = users.findIndex(u => u.email === email);
+                if (index >= 0) {
+                    users.splice(index, 1);
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end();
+                }
+                else {
+                    return handleError(res, 404);
+                }
+            }
+            else {
+                return handleError(res, 404);
+            }
+            break;
+    }
+});
+// Start the server
+server.listen(port, host, () => {
+    console.log("server is running on port 3000");
+});
+//handling error
+function handleError(res, code) {
+    res.statusCode = code;
+    res.end(`{"error"}`);
+}
