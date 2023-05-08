2a30fd4f9de1d80da1e6a5cc62b140f118e692b4
Jinal Mistry <jinalmistry1505@gmail.com>
2023-05-08 23:14:31 +0530

    Second Commit

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const url = require('url');
 const queryString = require('querystring');
 const host = "localhost";
 const port = "3000";
-const users = [
-    { email: "abc@gmail.com", name: "abc", age: 20 },
-    { email: "jinny@gmail.com", name: "Jinny", age: 22 }
-]
+const fs = require('fs');
+const file = "db.json";
+function readFile() {
+    const data = fs.readFileSync(file);
+    return JSON.parse(data);
+}
+function writeFile(data) {
+    fs.writeFileSync(file, JSON.stringify(data));
+}
+
 const server = http.createServer((req, res) => {
     // const method = req.method;
     const { pathname, query } = url.parse(req.url);
@@ -16,13 +22,15 @@ const server = http.createServer((req, res) => {
         // Handle GET requests==read
         case 'GET':
             if (pathname === '/') {
+                const userData = readFile();
                 res.setHeader('Content-Type', 'application/json');
                 res.writeHead(200);
-                res.end(JSON.stringify(users));
+                res.end(JSON.stringify(userData));
             }
             //handle GET perticuler user
             else if (pathname === '/user' && email) {
-                const user = users.find(u => u.email);
+                const userData = readFile();
+                const user = userData.find(u => u.email);
                 if (user) {
                     res.setHeader('Content-Type', 'application/json');
                     res.writeHead(200);
@@ -41,16 +49,18 @@ const server = http.createServer((req, res) => {
             if (pathname === '/') {
                 let b = '';
                 req.on('data', chunks => {
-                    b = b + chunks.toString();
+                    b += chunks.toString();
                 });
                 req.on('end', () => {
                     const user = JSON.parse(b);
-                    if (users.find(u => u.email === user.email)) {
-                        res.writeHead(409, { 'Content-Type': 'application/json' });
+                    const userData = readFile();
+                    if (userData.find(u => u.email === user.email)) {
+                        res.writeHead(404, { 'Content-Type': 'application/json' });
                         res.end();
                     }
                     else {
-                        users.push(user);
+                        userData.push(user);
+                        writeFile(userData);
                         res.writeHead(201, { 'Content-Type': 'application/json' });
                         res.end();
                     }
@@ -68,9 +78,11 @@ const server = http.createServer((req, res) => {
                 });
                 req.on('end', () => {
                     const user = JSON.parse(b);
-                    const index = users.findIndex(u => u.email === email);
+                    const userData = readFile();
+                    const index = userData.findIndex(u => u.email === email);
                     if (index >= 0) {
-                        users[index] = { ...user, email }
+                        userData[index] = { ...user, email }
+                        writeFile(userData);
                         res.writeHead(201, { 'Content-Type': 'application/json' });
                         res.end();
                     } else {
@@ -86,9 +98,11 @@ const server = http.createServer((req, res) => {
         //handle delete
         case 'DELETE':
             if (pathname === '/user' && email) {
-                const index = users.findIndex(u => u.email === email);
+                let userData = readFile();
+                const index = userData.findIndex(u => u.email === email);
                 if (index >= 0) {
-                    users.splice(index, 1);
+                    userData.splice(index, 1);
+                    writeFile(userData);
                     res.writeHead(200, { 'Content-Type': 'application/json' });
                     res.end();
                 }
